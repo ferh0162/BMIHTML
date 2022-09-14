c70f8b308bb13149f91eabb078cb3d8e787d7a8f
ferh0162 <ferh0162@stud.kea.dk>
2022-09-14 09:13:53 +0200

    Initial commit

---
+++ src/main/java/com/example/bmihtml/BmihtmlApplication.java
@@ -0,0 +1,13 @@
+package com.example.bmihtml;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BmihtmlApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(BmihtmlApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/example/bmihtml/BmihtmlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bmihtml;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BmihtmlApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
